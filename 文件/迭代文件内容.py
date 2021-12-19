c0f5e430d501c6b1c565014b6331839206e5ebbf
wuhengyu <578060214@qq.com>
2021-12-19 19:46:59 +0800

    文件

--- 文件/迭代文件内容.py
+++ 文件/迭代文件内容.py
@@ -1,7 +1,6 @@
 def processs(string):
     print('Processing:', string)
 
-
 # with open('diedai.txt') as f:
 #     char = f.read(1)
 #     while char:
@@ -9,10 +8,24 @@ def processs(string):
 #         char = f.read(1)
 
 
-with open('diedai.txt') as f:
-    while True:
-        char = f.read(1)
-        if not char:
-            break
-        processs(char)
+# with open('diedai.txt') as f:
+#     while True:
+#         char = f.read(1)
+#         if not char:
+#             break
+#         processs(char)
 
+# 使用read迭代字符
+# with open('diedai.txt') as f:
+#     for char in f.read():
+#         processs(char)
+
+# 使用readline迭代行
+with open('diedai.txt') as f:
+    for line in f.readline():
+        processs(line)
+
+# 使用readlines迭代全部行
+# with open('diedai.txt') as f:
+#     for line in f.readlines():
+#         processs(line)
